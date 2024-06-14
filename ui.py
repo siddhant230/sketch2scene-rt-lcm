f6afee392a44801953b62101342e357522b93380
siddhant230 <rsiddhant73@gmail.com>
2024-06-15 01:49:41 +0530

    added judge func

--- ui.py
+++ ui.py
@@ -10,10 +10,12 @@ from vlm import LLMUtility
 canvas_size = 512
 llm_obj = LLMUtility()
 
+
 def score_fn(image):
     global llm_obj
-    response, rating =  llm_obj.get_response_score(image)
+    response, rating = llm_obj.get_response_score(image)
     return response
+
 
 if not path.exists(cache_path):
     os.makedirs(cache_path, exist_ok=True)
@@ -24,25 +26,24 @@ with gr.Blocks() as demo:
     with gr.Column():
         with gr.Row():
             with gr.Column():
-                s = gr.Slider(label="steps", minimum=4, maximum=8,
+                s = gr.Slider(label="steps", minimum=1, maximum=6,
                               step=1, value=4, interactive=True)
                 c = gr.Slider(label="cfg", minimum=0.1, maximum=3,
                               step=0.1, value=1, interactive=True)
+
+            with gr.Column():
                 i_s = gr.Slider(label="sketch strength", minimum=0.1,
                                 maximum=0.9, step=0.1, value=0.9, interactive=True)
-            with gr.Column():
-                mod = gr.Text(label="Model Hugging Face id (after changing this wait until the model downloads in the console)",
-                              value="Lykon/dreamshaper-7", interactive=True)
                 t = gr.Text(
                     label="Prompt", value="8K, realistic, colorful art, natural", interactive=True)
             with gr.Column():
                 score = gr.Text(
-                                label="Judge", value="0/10", interactive=True)
+                    label="Judge", value="0/10", interactive=True)
         with gr.Row(equal_height=True):
             i = gr.Image(source="canvas", tool="color-sketch", shape=(canvas_size,
                          canvas_size), width=canvas_size, height=canvas_size, type="pil")
             o = gr.Image(width=canvas_size, height=canvas_size)
-            
+
             def process_image(p, im, steps, cfg, image_strength, seed):
                 if not im:
                     return Image.new("RGB", (canvas_size, canvas_size))
@@ -64,12 +65,6 @@ with gr.Blocks() as demo:
                 control.change(fn=process_image,
                                inputs=reactive_controls, outputs=[o, score])
 
-            def update_model(model_name):
-                global infer
-                infer = load_models(model_name)
-
-            mod.change(fn=update_model, inputs=mod)
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -78,4 +73,3 @@ if __name__ == "__main__":
                         help="Deploy on Gradio for sharing", default=False)
     args = parser.parse_args()
     demo.launch(share=args.share)
-
